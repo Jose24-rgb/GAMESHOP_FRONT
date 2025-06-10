25416f6bdf9d74c200e84c9a71b9df3710c757e0
Makobyfull <Jose_r_licona12@hotmail.com>
2025-06-10 17:43:32 +0200

    modifiche

--- src/services/apis.js
+++ src/services/apis.js
@@ -4,13 +4,15 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token && token !== 'undefined') {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => Promise.reject(error));
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token && token !== 'undefined') {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
-
